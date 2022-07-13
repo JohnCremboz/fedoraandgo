08cdf0b1685c3f2bd52e69ccd00ac4cb160d3c77
JohnCremboz <103947481+JohnCremboz@users.noreply.github.com>
2022-07-13 09:07:23 +0200

    Update fedoraandgo.sh

--- fedoraandgo.sh
+++ fedoraandgo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo dnf upgrade
+sudo dnf install fedora-workstation-repositories
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core
 sudo dnf groupupdate multimedia
@@ -11,6 +12,11 @@ sudo dnf install wine
 sudo dnf install steam
 sudo dnf install variety
 sudo dnf install blender
+sudo dnf install solaar
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
 flatpak install teams
